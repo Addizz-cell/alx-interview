d89be4b51d91a2c507d0f25bdb972f9fd9621164
Addizz-cell <iwayemiolawale@gmail.com>
2024-08-01 07:09:32 -0400

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Solves the lock boxes puzzle """
+"""Solves the lock boxes puzzle."""
 
 
 def look_next_opened_box(opened_boxes):
-    """Looks for the next opened box
+    """Looks for the next opened box.
+    
     Args:
-        opened_boxes (dict): Dictionary which contains boxes already opened
+        opened_boxes (dict): Dictionary which contains boxes already opened.
+    
     Returns:
-        list: List with the keys contained in the opened box
+        list: List with the keys contained in the opened box.
     """
     for index, box in opened_boxes.items():
         if box.get('status') == 'opened':
@@ -17,11 +19,13 @@ def look_next_opened_box(opened_boxes):
 
 
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened
+    """Check if all boxes can be opened.
+    
     Args:
-        boxes (list): List which contain all the boxes with the keys
+        boxes (list): List which contain all the boxes with the keys.
+    
     Returns:
-        bool: True if all boxes can be opened, otherwise, False
+        bool: True if all boxes can be opened, otherwise, False.
     """
     if len(boxes) <= 1 or boxes == [[]]:
         return True
@@ -37,8 +41,7 @@ def canUnlockAll(boxes):
         if keys:
             for key in keys:
                 try:
-                    if aux.get(key) and aux.get(key).get('status') \
-                       == 'opened/checked':
+                    if aux.get(key) and aux.get(key).get('status') == 'opened/checked':
                         continue
                     aux[key] = {
                         'status': 'opened',
@@ -57,7 +60,7 @@ def canUnlockAll(boxes):
 
 
 def main():
-    """Entry point"""
+    """Entry point."""
     canUnlockAll([[]])
 
 
